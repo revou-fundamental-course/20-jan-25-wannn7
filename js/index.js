4b578667f088288fd6b5e28d6bbd270a6ebd30ae
Iwan <azzurridwan05@gmail.com>
2025-01-28 13:24:00 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function hitung() {
+	const Tb = document.getElementById("tinggi").value;
+	const Bb = document.getElementById("berat").value;
+
+	const tb = (Tb / 100) * (Tb / 100);
+	const result = Bb / tb;
+	document.getElementById("result").innerText = result.toFixed(1);
+}
+
+function reset() {
+	const result = document.getElementById("result");
+	tinggi.value = "";
+	berat.value = "";
+	umur.value = "";
+	result.value = "";
+}
